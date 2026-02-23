b0d5b58672ddfc5f5ad141d56dd3fc33b877bc65
Chaitanya Asati <chaitanya.asati1@gmail.com>
2026-02-23 16:57:42 +0530

    added priority queue

--- src/main/java/com/fundamentals/collectionsframework/queuedemo/PriorityQueueDemo.java
+++ src/main/java/com/fundamentals/collectionsframework/queuedemo/PriorityQueueDemo.java
@@ -1,7 +1,15 @@
 package com.fundamentals.collectionsframework.queuedemo;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class PriorityQueueDemo {
     public static void main(String[] args) {
-        f
+        Queue<String> stringLength = new PriorityQueue<>(Comparator.comparingInt(String::length));
+        stringLength.add("Shubham");
+        stringLength.add("Akshit");
+        stringLength.add("Neha");
+        System.out.println(stringLength);
     }
 }
